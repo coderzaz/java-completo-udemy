cfa44dffcb05d5a99a0aadea442f35f2b3ec37fb
Paulo <coderzaz@gmail.com>
2021-11-17 10:01:59 -0300

    Upload Inicial

---
+++ JavaCompletoUdemy/src/br/com/coderzaz/udemy/secao4/aula25/Exec2.java
@@ -0,0 +1,18 @@
+package br.com.coderzaz.udemy.secao4.aula25;
+
+public class Exec2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x;
+		double y;
+		
+		x = 5;
+		y = 2 * x;
+		
+		System.out.println(x);
+		System.out.println(y);
+
+	}
+
+}
