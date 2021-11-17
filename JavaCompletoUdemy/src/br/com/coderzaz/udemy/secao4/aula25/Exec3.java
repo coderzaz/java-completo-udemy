ef2a622b83e56967779812762107cc691852ff3e
Paulo <coderzaz@gmail.com>
2021-11-17 10:42:27 -0300

    Aula 25 casting

---
+++ JavaCompletoUdemy/src/br/com/coderzaz/udemy/secao4/aula25/Exec3.java
@@ -0,0 +1,23 @@
+package br.com.coderzaz.udemy.secao4.aula25;
+
+public class Exec3 {
+/**
+ * Calculo área do trapézio 
+ * @param args
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double b,B,h,area;
+		
+		b = 6.0;
+		B = 8.0;
+		h = 5.0;
+		
+		area = (b + B) / 2.0 *h;
+		System.out.println(area);
+		
+		
+	}
+
+}
---
+++ JavaCompletoUdemy/src/br/com/coderzaz/udemy/secao4/aula25/Exec4.java
@@ -0,0 +1,22 @@
+package br.com.coderzaz.udemy.secao4.aula25;
+
+public class Exec4 {
+/**
+ * Casting
+ * A divisão de dois números inteiro resulta num número inteiro
+ * @param args
+ */
+	public static void main(String[] args) {
+		int a,b;
+		double resultado;
+		
+		a = 5;
+		b = 2;
+		
+		resultado = (double) a / b;
+		
+		System.out.println(resultado);
+
+	}
+
+}
